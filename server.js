17570a302f5c24ddf22624312fa2422d1cac0310
pranu88 <pranusha.pothugunta@gmail.com>
2023-12-14 11:37:01 -0500

    Magic 8 ball express application created

--- server.js
+++ server.js
@@ -16,11 +16,20 @@ app.get("/greeting", (req,res)=>{
     })
 
     app.get('/tip/:total/:tipPercentage', (req, res)=> {
-        let percentage = (req.params.total)*(req.params.tipPercentage / 100)
-        res.send(`<h1>Your tip amount: ${percentage} </h1>`);
+        let tipPercentage = ((req.params.total)*(req.params.tipPercentage))/ 100
+        res.send(`<h1>Your tip amount: ${tipPercentage} </h1>`);
       });
+ 
 
 
+      let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+      app.get('/magic/:question',(req,res)=>{
+      let random = Math.floor(Math.random()*(19-0)+0)
+          
+       res.send(`Question: ${req.params.question}, Answer: <h1>${answers[random]}</h1>`)
+        
+
+      })
 
 
 
